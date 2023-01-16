ae6c7f874b1070d3031dc88b7dc0abf13b023e9a
javimoreeno <115158222+javimoreeno@users.noreply.github.com>
2023-01-16 14:43:34 +0100

    Update ej1_listas.js

--- ej1_listas.js
+++ ej1_listas.js
@@ -1,13 +1,36 @@
-    // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+     // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+    var lista_pares = [];
+    var i = 0;
+    while(i<=20){
+        if(i%2==0){
+            lista_pares.push(i);
+        }
+       i = i + 1;
+
+    }
+    console.log("Pares: " + lista_pares);
 
     // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
-    
+    var lista_impares = [];
+
+    for( var index = 1 ; index <= 19; index++){
+        if(index % 2 == 1){
+            lista_impares.push(index);
+        }
+    }
+    console.log("Impares: " + lista_impares);
+
     // Crea una lista de comida.
+    var lista_comida = [" tortilla", " durum", " paella"];
+    console.log("Comidas: " + lista_comida);
     
+
     // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
-    
+    lista_comida.unshift("Pizza");
+    console.log("Mas comidas: " + lista_comida);
     // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
-    
+    lista_comida[1]= "Puchero";
+    console.log("Comidas: " + lista_comida);
     // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
     
     // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
@@ -19,3 +42,4 @@
     // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
     
     // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
+
